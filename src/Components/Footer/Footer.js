7969dd277e3bda238679b128507410b6a137e9ad
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-07-14 16:08:34 -0500

    Set up footer w/ <CityLogo />

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
+import { CityLogo } from '../Utils/tools'
+
 const Footer = () => {
   return (
-    <div>
-      <h1>footer</h1>
-    </div>
+    <footer className="bck_blue">
+      <div className="footer_logo">
+        <CityLogo //
+          link={true}
+          linkTo={'/'}
+          width="70px"
+          height="70px"
+        />
+      </div>
+      <div className="footer_desc1">Manchester City 2021. All rights reserved.</div>
+    </footer>
   )
 }
 
